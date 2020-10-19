94939a8460ca870ac61cb0538119684f3e1c8f42
sung <sung@esports-pro.com>
2020-10-19 13:29:47 +0800

    feat

--- src/LeagueStatus.php
+++ src/LeagueStatus.php
@@ -4,6 +4,7 @@ namespace EsportsLolEnum;
 
 class LeagueStatus extends AbstractEnum
 {
+    const ALL = 'null';
     const UPCOMING  = 0;
     const RUNNING   = 1;
     const PAST      = 2;
--- src/MatchesStatus.php
+++ src/MatchesStatus.php
@@ -3,6 +3,7 @@ namespace EsportsLolEnum;
 
 class MatchesStatus extends AbstractEnum
 {
+    const ALL = 'null';
     const UPCOMING  = 0;
     const RUNNING   = 1;
     const PAST      = 2;
